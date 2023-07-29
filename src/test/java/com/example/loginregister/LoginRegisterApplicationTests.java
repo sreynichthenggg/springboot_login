5d831dc22dccdf64f24751d547c93926529cac86
Theng-Sreynich <sreynichtheng547@gmail.com>
2023-07-29 21:44:26 +0700

    first

---
+++ src/test/java/com/example/loginregister/LoginRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
